e2723e39b08caf19f51c203095fc79beb15c3037
ptepmpt <148393456+ptepmpt@users.noreply.github.com>
2025-07-27 21:32:20 +0300

    wood

--- src/main/java/com/example/hooktest/DevastController.java
+++ src/main/java/com/example/hooktest/DevastController.java
@@ -24,7 +24,7 @@ public class DevastController {
 
 
         //recursivePrint(body);
-       return "{\"type\":\"commands\", \"content\": [\"!build=wood_wall:149:149\"]}";
+       return "{\"type\":\"commands\", \"content\": [\"!build=steel_wall:149:149\"]}";
 
         //return "{ \"type\": \"filter\", \"content\": 0 }";
     }

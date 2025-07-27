bcb4f68c7ae8c2109a03f469df5602f8214988f3
ptepmpt <148393456+ptepmpt@users.noreply.github.com>
2025-07-27 21:13:57 +0300

    123

--- src/main/java/com/example/hooktest/DevastController.java
+++ src/main/java/com/example/hooktest/DevastController.java
@@ -10,14 +10,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/devast")
 public class DevastController {
+    int counter = 0;
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     String doHook(@RequestBody(required = false) Map<String,Object> body) {
-        System.out.println("POST");
-        System.out.println(body);
-        return "{\"type\":\"commands\", \"content\": [\"!build=wood_wall:149:149\", \"!item-to=16:wood\"]}";
+        System.out.println("POST" + counter++);
+        //System.out.println(body);
+
+
+        //recursivePrint(body);
+       return "{\"type\":\"commands\", \"content\": [\"!build=steel_wall:149:149\"]}";
+        //return "{ \"type\": \"filter\", \"content\": 0 }";
+    }
+
+    void recursivePrint(Map<String,Object> body){
+        for (Map.Entry<String, Object> entry : body.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                recursivePrint((Map<String,Object>)entry.getValue());
+                continue;
+            }
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
